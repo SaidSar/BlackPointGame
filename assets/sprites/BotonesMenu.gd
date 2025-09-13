3294063185c4bdd9fe74c085abcdeaac0041a902
Said <groserito97@gmail.com>
2025-09-12 19:07:21 -0700

    nivel de las catacumbas

---
+++ assets/sprites/BotonesMenu.gd
@@ -0,0 +1,20 @@
+extends VBoxContainer
+
+func _ready():
+	for boton in get_children():
+		if boton is Button:
+			configurar_glow_boton(boton)
+
+func configurar_glow_boton(boton: Button):
+	var color_original = boton.modulate
+	
+	boton.mouse_entered.connect(func():
+		var tween = create_tween()
+		# Efecto de brillo dorado
+		tween.tween_property(boton, "modulate", Color(1.2, 1.0, 0.3, 1.0), 0.3)
+	)
+	
+	boton.mouse_exited.connect(func():
+		var tween = create_tween()
+		tween.tween_property(boton, "modulate", color_original, 0.3)
+	)
