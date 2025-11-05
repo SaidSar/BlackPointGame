8d5bc5336a3e3ce3248c1d9c7b2cadf9fc4e2894
Divinepath6 <132866475+Divinepath6@users.noreply.github.com>
2025-11-05 10:17:08 -0700

    0.0..0.0.0.0.0.0.0.04.8

--- Codigo/Otras cosas/Enemigo_melee.gd
+++ Codigo/Otras cosas/Enemigo_melee.gd
@@ -3,9 +3,12 @@ var velocidad: int
 var dir: Vector2
 var perseguido: bool
 var vida: int
-var daño = 2
+var daño = 4
 var atacando: bool
+var tamaño_ray_activo : float
+var tamaño_ray_desactivado : float
 
+var animacion_atacando = false
 @onready var colision_ataque
 @onready var sprite = $Sprite
 @onready var barra_vida = $BarraVida
@@ -16,15 +19,12 @@ var atacando: bool
 @onready var raycast_izquierda = $Izquierda
 @onready var area = $"Area"
 
-var tiempo_carga: float = 0.4
-const CARGA_MAX = 1.5 
-const FUERZA_MAX = 1200.0  
-const FUERZA_MIN = 400.0   
-
 func _ready():
+	tamaño_ray_activo = 180
+	tamaño_ray_desactivado = 40
 	atacando = false
 	perseguido = false
-	velocidad = 50
+	velocidad = 60
 	vida = 15
 	barra_vida.iniciar_vida(vida)
 	barra_vida._set_vida(vida)
@@ -35,19 +35,46 @@ func _process(delta):
 		velocity += get_gravity() * delta
 	velocity.x = dir.x * velocidad
 	voltear_sprite(dir)
-	if !perseguido:
-		if raycast_derecha.is_colliding() and !atacando:
-			var objetivo = raycast_derecha.get_collider()
-			if objetivo and objetivo.is_in_group("Jugadores"):
+	
+	
+	var objetivo_derecha = raycast_derecha.get_collider()
+	var objetivo_izquierda = raycast_izquierda.get_collider()
+	if raycast_derecha.is_colliding()  and objetivo_derecha.is_in_group("Jugadores"):
+		perseguido = false
+		voltear_sprite( Vector2.RIGHT)
+		var collision_point: Vector2 = raycast_derecha.get_collision_point()
+		var origin_point: Vector2 = global_position
+		var distancia: float = origin_point.distance_to(collision_point)
+		if distancia >= 45:
+			dir = Vector2.RIGHT
+			if !atacando:
+				velocidad = 60
+			else:
+				velocidad = 40
+		elif !atacando:
 				atacar(Vector2.RIGHT)
-		if raycast_izquierda.is_colliding() and !atacando:
-			var objetivo = raycast_izquierda.get_collider()
-			if objetivo and objetivo.is_in_group("Jugadores"):
+	elif raycast_izquierda.is_colliding()  and objetivo_izquierda.is_in_group("Jugadores"):
+		voltear_sprite( Vector2.LEFT)
+		perseguido = false
+		var collision_point: Vector2 = raycast_izquierda.get_collision_point()
+		var origin_point: Vector2 = global_position
+		var distancia: float = origin_point.distance_to(collision_point)
+		if distancia >= 45:
+			dir = Vector2.LEFT
+			if !atacando:
+				velocidad = 60
+			else:
+				velocidad = 40
+		elif !atacando:
 				atacar(Vector2.LEFT)
+	else:
+		Vigilar()
 	move_and_slide()
+	Controlador_animaciones(dir)
+
 
 func Controlador_animaciones(dir):
-	if !atacando:
+	if !animacion_atacando:
 		if is_on_floor():
 			if !velocity:
 				sprite.play("Parado")
@@ -58,19 +85,29 @@ func Controlador_animaciones(dir):
 			sprite.play("Callendo")
 
 func voltear_sprite(dir):
-	if dir == 1:
+	if dir.x == 1:
 		sprite.flip_h = false
 		area.scale.x = 1
-	if dir == -1:
+		raycast_derecha.target_position.x = tamaño_ray_activo
+		raycast_izquierda.target_position.x = -tamaño_ray_desactivado
+	if dir.x == -1:
 		sprite.flip_h = true
 		area.scale.x = -1
+		raycast_izquierda.target_position.x = -tamaño_ray_activo
+		raycast_derecha.target_position.x = tamaño_ray_desactivado
+		
 
+func Vigilar():
+	velocidad = 25
+	perseguido = true
+	timer_2.wait_time =  choose([ 8.6, 8.5, 8.3, 8.4, 8.7])
+	timer_2.start()
+	timer_1.wait_time = 0.1
+	timer_1.start()
 
 func _on_timer_timeout():
-	if timer_2.is_stopped():
-			perseguido = false
 	if perseguido:
-		timer_1.wait_time = choose([ 2.6, 2.5, 2.3, 2.4, 1.7])
+		timer_1.wait_time = choose([ 8.6, 8.5, 8.3, 8.4, 8.7])
 		dir = choose([Vector2.RIGHT, Vector2.LEFT, Vector2.RIGHT, Vector2.LEFT])
 	else:
 		timer_1.stop()
@@ -81,31 +118,32 @@ func choose(array):
 	return array.front()
 
 func recibir_daño(daño):
-	perseguido = true
-	timer_2.wait_time = choose([ 4.0, 3.8, 4.2, 4.6])
-	timer_2.start()
-	timer_1.wait_time = 0.1
-	timer_1.start()
 	vida -= daño
 	if vida <= 0:
 		queue_free()
 	barra_vida._set_vida(vida)
-	print("daño recibido: ", daño)
 
 func _on_timer_2_timeout() -> void:
 	timer_2.stop()
 
 func atacar(direccion: Vector2):
+	animacion_atacando = true
 	atacando = true
-	Controlador_animaciones(dir)
-	var espera = 0.2
+	sprite.play("Ataque_1")
+	var espera = 0.3
 	await get_tree().create_timer(.2).timeout
-	area.disabled = false
+	colision_ataque.disabled = false
 	await get_tree().create_timer(espera).timeout
-	area.disabled = true
+	colision_ataque.disabled = true
 	ataque_timer.start()
+	animacion_atacando = false
 
 func _on_ataque_timeout() -> void:
 	atacando = false
 	ataque_timer.stop()
 	ataque_timer.wait_time = choose([ 5.0, 4.8, 4.4, 3.8, 4.0, 5.2, 4.2])
+
+
+func _on_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Jugadores"): 
+		body.recibir_daño(daño) 
