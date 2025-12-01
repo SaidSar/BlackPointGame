565dfcf03d349fad4eaf63315fb78f53c28bf8b4
Divinepath6 <132866475+Divinepath6@users.noreply.github.com>
2025-12-01 08:41:28 -0700

    0.0.0.0.0.0.0.9.9

--- Codigo/Otras cosas/puerta.gd
+++ Codigo/Otras cosas/puerta.gd
@@ -1,16 +1,19 @@
 extends Node2D
 @export var mensaje : Control
-var mostrar_mensaje : bool = true:
+@onready var mostrar_mensaje : bool = false:
 	set(value):
 		mostrar_mensaje = value
 		if value:
 			mensaje.visible = true
 		else:
 			mensaje.visible = false
+func _ready() -> void:
+	mostrar_mensaje = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Jugadores"):
 		mostrar_mensaje = true
+	
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Jugadores"):
